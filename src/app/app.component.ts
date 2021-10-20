5edfb59124496c43c7a1f5aaa7e135c86634f72e
kaushic <kaushic98@gmail.com>
2021-10-20 15:47:48 -0400

    basic form handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'canmydogeatthis';
+  @ViewChild('food') foodForm!: NgForm;
+  resetForm() {
+    this.foodForm.reset();
+  }
+  onSubmit(value: any) {
+    // console.log(value.search);
+    if (value.search === "food") {
+      alert("I'm gonna need you to be a little more specific");
+    } else {
+      this.setTitle(value.search);
+      // console.log("resetting");
+    }
+  }
+
+  setTitle(food: any) {
+    let heading;
+    if (food) {
+      heading = "not empty";
+    } else {
+      heading = "empty";
+    }
+    console.log(heading);
+
+    // console.log(this.question);
+  }
 }
