64443f1a639edd369f191c7eb6f4bda2b82ee391
kaushic <kaushic98@gmail.com>
2021-10-21 09:53:28 -0400

    generate gif response

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -65,18 +65,36 @@ export class AppComponent {
     if (decision) {
       resp = this.getRandomYesResponse();
     } else {
-      resp = "that's a no from me dawg";
+      resp = this.getRandomNoResponse();
     }
     this.renderDecision(resp);
   }
   renderDecision(resp: any) {
-    this.decision.nativeElement.innerHTML = `<div style="width:100%;height:0;padding-bottom:83%;position:relative;"><img src="src/yesGifs/yes1.gif"></img></div>`;
+    this.decision.nativeElement.innerHTML = resp;
     // <iframe src="https://giphy.com/embed/MNmyTin5qt5LSXirxd" width="50%" height="50%" style="position:absolute" frameBorder="0" class="giphy-embed"></iframe>
     //resp;//`<h2 class="response">"${resp}"</h2>`;
 
   }
 
   getRandomYesResponse() {
-    console.log("h i");
+    let arr = [
+      `<div style="width:50%;height:0;padding-bottom:83%;position:relative;margin-left: auto;margin-right: auto;"><img src="yesGifs/yes1.gif"></img></div>`,
+      `<div style="width:50%;height:0;padding-bottom:83%;position:relative;margin-left: auto;margin-right: auto;"><img src="yesGifs/yes2.gif"></img></div>`,
+      `<div style="width:50%;height:0;padding-bottom:83%;position:relative;margin-left: auto;margin-right: auto;"><img src="yesGifs/yes3.gif"></img></div>`,
+      `<div style="width:50%;height:0;padding-bottom:83%;position:relative;margin-left: auto;margin-right: auto;"><img src="yesGifs/yes4.gif"></img></div>`,
+      `<div style="width:50%;height:0;padding-bottom:83%;position:relative;margin-left: auto;margin-right: auto;"><img src="yesGifs/yes5.gif"></img></div>`,
+    ]
+
+    return arr[Math.floor(Math.random() * arr.length)]
+  }
+  getRandomNoResponse() {
+    let arr = [
+      `<div style="width:50%;height:0;padding-bottom:83%;position:relative;margin-left: auto;margin-right: auto;"><img src="noGifs/no1.gif"></img></div>`,
+      `<div style="width:50%;height:0;padding-bottom:83%;position:relative;margin-left: auto;margin-right: auto;"><img src="noGifs/no2.gif"></img></div>`,
+      `<div style="width:50%;height:0;padding-bottom:83%;position:relative;margin-left: auto;margin-right: auto;"><img src="noGifs/no3.gif"></img></div>`,
+      `<div style="width:50%;height:0;padding-bottom:83%;position:relative;margin-left: auto;margin-right: auto;"><img src="noGifs/no4.gif"></img></div>`,
+      `<div style="width:50%;height:0;padding-bottom:83%;position:relative;margin-left: auto;margin-right: auto;"><img src="noGifs/no5.gif"></img></div>`,
+    ]
+    return arr[Math.floor(Math.random() * arr.length)]
   }
 }
